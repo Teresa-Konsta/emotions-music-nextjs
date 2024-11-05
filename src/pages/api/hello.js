940e14b1cf6c479714b59d029e9af26c65ffd49b
Tereza Konstari <teresa.konstari@gmail.com>
2024-11-04 19:56:54 -0800

    updating

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//I'm leaving this REST API file on purpose
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
