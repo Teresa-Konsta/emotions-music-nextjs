a1cdf42ee589f9a84c8c6db75b11a84d1599ee87
Tereza Konstari <teresa.konstari@gmail.com>
2025-02-01 21:12:17 -0800

    Update Instruction.js

--- src/components/guide/Instruction.js
+++ src/components/guide/Instruction.js
@@ -9,8 +9,8 @@ export default function Instruction() {
             <p>
                 Three music notes are generated based on your current pulse rate.
                 To improve your mood it is recommended to listen to some music not in the same vibe as these notes,
-                and not in the opposite, but a &ldquo;in the middle&rdquo;.
-                For example, if prototypes generate you &ldquo;sad&rdquo; notes,
+                and not in the opposite, but &ldquo;in the middle&rdquo;. 
+                For example, if a prototype generate you &ldquo;sad&rdquo; notes,
                 minimalistic, jazz, soul or calm music should improve your mood.
             </p>
         </div>
